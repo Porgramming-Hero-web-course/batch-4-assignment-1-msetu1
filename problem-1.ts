1835e9353146ab50bca9e484bdcae7ea7280bf67
msetu1 <msetu5763@gmail.com>
2024-11-08 15:24:50 +0600

    problem-1 are solve

---
+++ problem-1.ts
@@ -0,0 +1,11 @@
+{
+    //problem-1
+    function sumArray(numbers: number[]): number {
+      return numbers.reduce((acc, currentValue) => acc + currentValue, 0);
+    }
+    // const myNumber: number[] = [4, 7, 9, 3, 1, 20];
+    // console.log(sumArray(myNumber));
+  
+    //
+  }
+  
